332debfe18bb7d4376a2b2bde32043dc7e35cb2a
Ahmed Assal <aassal@kth.se>
2017-09-19 22:41:42 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name='DataGenerators',
+  version='0.1.0',
+  packages=['DataGenerators.DataGenerators'],
+  url='https://github.com/ahmedassal/DataGenerators',
+  license='MIT',
+  author='Ahmed Assal',
+  author_email='ahmed.assal@vertex-techs.com',
+  description='DataGenerators'
+)
